36e2762d37e83974ff9fa03d1598128bc7a09084
Merchelago <tomreddle00@mail.ru>
2023-08-31 15:29:14 +0300

    feat: develop logic

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-   
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private int width;
+    [SerializeField]
+    private int height;
+    [SerializeField]
+    private GameObject spawnPoint;
+    [SerializeField]
+    private Tile[,] grid;
+
+    private void Start()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        Vector3 tr = spawnPoint.transform.position;
+        for(int y = 0; y <= height; y++)
+        {
+            for(int x = 0; x <= width; x++)
+            {
+                grid = new Tile[height, width];
+                var cell = Instantiate(prefab, tr, Quaternion.identity);
+                Vector2Int vector2Int = new Vector2Int(y, x);
+                //grid[y, x] = (Tile)cell;
+                grid[y, x].coordinates = vector2Int;
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public Vector2Int coordinates;
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
