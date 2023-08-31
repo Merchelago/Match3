aca619e6601bfdd66dcdc5fa117d27d13bfb8ab6
Merchelago <tomreddle00@mail.ru>
2023-08-31 23:40:11 +0300

    update scripts

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -23,30 +23,32 @@ public class Board : MonoBehaviour
 
     private void CreateGrid()
     {
-        Vector3 tr = gameObject.transform.localPosition;
+        grid = new Tile[height, width];
+        
+        
         for (int y = 0; y < height; y++)
         {
             for(int x = 0; x < width; x++)
             {
-                grid = new Tile[height, width];
-                GameObject pref = CreatePrefabs(tr);
+                
+                GameObject pref = CreateCell();
                 pref.transform.SetParent(gameObject.transform, false);
                 var tile = pref.GetComponent<Tile>();
                 var rect = pref.GetComponent<RectTransform>();
 
-                rect.position = new Vector3(spawnPoint.position.x+x, spawnPoint.position.y+y);
+                //rect.position = new Vector3(vect.x+x, vect.y+y*-1);
                 
-                Vector2Int vector2Int = new Vector2Int(y, x);
+                
                 grid[y, x] = tile;
-                grid[y, x].coordinates = vector2Int;
+                grid[y, x].coordinates = new(x,y);
             }
         }
     }
 
-    private GameObject CreatePrefabs(Vector3 tr)
+    private GameObject CreateCell()
     {
         
-         _prefab = Instantiate(prefab, tr, Quaternion.identity);
+        _prefab = Instantiate(prefab);
         return _prefab;
     }
 }
